cb8fb3ecd75ea3b600f150ba453a94cf693b1ed9
ih <irvin.hwang@gmail.com>
2013-04-28 14:20:10 -0700

    added starter files for the server/client for sending data back and forth between a computer that can connect to the mindwave and the raspberry pi

---
+++ mindwave_client.py
@@ -0,0 +1,26 @@
+#based http://docs.python.org/2/library/socketserver.html
+#this client is run on a computer where the mindwave works and sends
+#data to a server on the raspberry pi 
+
+
+import socket
+import sys
+
+HOST, PORT = "localhost", 9999
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(data + "\n")
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
---
+++ pi_server.py
@@ -0,0 +1,32 @@
+#based http://docs.python.org/2/library/socketserver.html
+#this server is run on the raspberry pi and receives info from a client
+#hooked up to the mindwave
+
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """b
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
