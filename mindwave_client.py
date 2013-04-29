e8b57302f0ed9647b91273ecb3a32b9adddcc043
ih <irvin.hwang@gmail.com>
2013-04-28 19:49:35 -0700

    modified server to hopefully take multiple messages from client on same connection

--- mindwave_client.py
+++ mindwave_client.py
@@ -1,26 +1,69 @@
 #based http://docs.python.org/2/library/socketserver.html
 #this client is run on a computer where the mindwave works and sends
-#data to a server on the raspberry pi 
+#data to a server on the raspberry pi
 
 
 import socket
 import sys
+import time
+import mindwave
+import pdb
+def connect_to_headset(headset):
+    while headset.status != 'connected':
+        time.sleep(0.5)
+        if headset.status == 'standby':
+            headset.connect()
+            print "Retrying connect..."
+        else:
+            print "mindwave status: %s" % headset.status
+    return
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect_to_pi():
+    #192.168.0.8
+    HOST, PORT = '192.168.0.8', 9999
 
-try:
-    # Connect to server and send data
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+    return sock
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-finally:
-    sock.close()
 
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+
+
+def send_data(sock):
+    try:
+        # Connect to server and send data
+        for i in range(1000000):
+            print i
+            data = 'attention:%s meditation:%s' % (headset.attention,
+                                                   headset.meditation)
+            sock.sendall(data + "\n")
+            #print data
+            # Receive data from the server and shut down
+            received = sock.recv(1024)
+            print "Sent:     {}".format(data)
+            print "Received: {}".format(received)
+
+        # data = 'attention:%s meditation:%s' % (headset.attention,
+        #                                        headset.meditation)
+        # sock.sendall(data + "\n")
+        #     #print data
+
+        # # Receive data from the server and shut down
+        # received = sock.recv(1024)
+        # print "Sent:     {}".format(data)
+        # print "Received: {}".format(received)
+
+
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    # connect to mindwave
+    headset = mindwave.Headset('/dev/tty.MindWave')
+
+    connect_to_headset(headset)
+    sock = connect_to_pi()
+    send_data(sock)
--- pi_server.py
+++ pi_server.py
@@ -5,7 +5,7 @@
 import SocketServer
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """b
+    """
     The RequestHandler class for our server.
 
     It is instantiated once per connection to the server, and must
@@ -15,14 +15,16 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        while self.data != '':
+            self.data = self.request.recv(1024).strip()
+            print "{} wrote:".format(self.client_address[0])
+            print self.data
+            # just send back the same data, but upper-cased
+            self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    #HOST should be the ip address of the raspberry pi
+    HOST, PORT = "192.168.0.8", 9999
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
