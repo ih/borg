dee974e6585ed217d49f76a4de12042c7cff3487
ih <irvin.hwang@gmail.com>
2013-04-28 22:30:01 -0700

    modified pi server to turn on/off leds based on mindwave data

--- pi_server.py
+++ pi_server.py
@@ -3,6 +3,11 @@
 #hooked up to the mindwave
 
 import SocketServer
+import RPi.GPIO as GPIO
+import pdb
+
+GREEN_LED = 18
+RED_LED = 23
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
@@ -14,17 +19,37 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
 
     def handle(self):
+        def parse_mindwave(data):
+            return [int(x) for x in data.split(',')]
+
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         while self.data != '':
             print "{} wrote:".format(self.client_address[0])
             print self.data
+            attention, meditation = parse_mindwave(self.data)
+            if attention > 50:
+                GPIO.output(RED_LED, True)
+                GPIO.output(GREEN_LED, False)
+            else:
+                GPIO.output(GREEN_LED, True)
+                GPIO.output(RED_LED, False)
+
             # just send back the same data, but upper-cased
             self.request.sendall(self.data.upper())
             if self.data != '':
                 self.data = self.request.recv(1024).strip()
 
+
+
+def initialize_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GREEN_LED, GPIO.OUT)
+    GPIO.setup(RED_LED, GPIO.OUT)
+
 if __name__ == "__main__":
+    initialize_gpio()
+
     #HOST should be the ip address of the raspberry pi
     HOST, PORT = "192.168.0.8", 9999
 
